40d35b8054fde403523cf871d9b859c2d083f3b9
sunyrora <sunyrora@gmail.com>
2022-06-21 07:18:48 +0200

    new - AddProjectModal,   - add mutation & module
    -  delete project mutation & module
    - delete & add button in projects screen

--- frontend/src/Components/Project.jsx
+++ frontend/src/Components/Project.jsx
@@ -1,8 +1,13 @@
-const Project = ({ project }) => {
+const Project = ({ project, deletHandler }) => {
   return (
-    <div className="col mb-3 col-sm-3">
+    <div className="col mb-3 col-sm-4">
       <div className="card m-1 h-100">
-        <div className="card-header text-truncate">{project.name}</div>
+        <a
+          href={`/project/${project.id}`}
+          className="project-name w-100 d-flex flex-column justify-content-center"
+        >
+          <div className="card-header text-truncate">{project.name}</div>
+        </a>
         <div className="card-body d-flex d-flex flex-column justify-content-start align-items-center p-1">
           <a
             href={`/project/${project.id}`}
@@ -17,6 +22,15 @@ const Project = ({ project }) => {
               {project.status}
             </span>
           </div>
+        </div>
+        <div className="card-footer d-flex justify-content-center">
+          <button className="btn btn-outline-success small">Edit</button>
+          <button
+            className="btn btn-outline-success small"
+            onClick={(e) => deletHandler(e, project.id)}
+          >
+            Delete
+          </button>
         </div>
       </div>
     </div>
--- frontend/src/Containers/ProjectsScreen.jsx
+++ frontend/src/Containers/ProjectsScreen.jsx
@@ -1,31 +1,105 @@
-import { useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
+import AddProjectModal from "../Components/AddProjectModal";
 import Project from "../Components/Project";
 import Spinner from "../Components/Spinner";
+import { ADD_PROJECT, DELETE_PROJECT } from "../mutations/projectMutations";
 import { GET_PROJECTS } from "../queries/projectQueries";
 
 const ProjectsScreen = () => {
   const {
     loading: projectsLoading,
-    error,
+    error: projectsError,
     data: projectsDdata,
   } = useQuery(GET_PROJECTS);
   const [loading, setLoading] = useState(projectsLoading);
+  const [error, setError] = useState("");
+
+  const [addProject, { loading: addLoading, error: addError }] = useMutation(
+    ADD_PROJECT,
+    {
+      update(cache, { data: { addProject } }) {
+        cache.modify({
+          fields: {
+            projects(existing = []) {
+              const newProject = cache.writeFragment({
+                data: addProject,
+                fragment: gql`
+                  fragment newProject on Project {
+                    id
+                    name
+                    description
+                    status
+                    client {
+                      id
+                    }
+                  }
+                `,
+              });
+              return [...existing, newProject];
+            },
+          },
+        });
+      },
+    }
+  );
+
+  const [deleteProject] = useMutation(DELETE_PROJECT, {
+    update: (cache, { data: { deleteProject } }) => {
+      const normalizedId = cache.identify({
+        id: deleteProject.id,
+        __typename: "Project",
+      });
+      cache.evict({ id: normalizedId });
+      cache.gc();
+    },
+  });
+
   useEffect(() => {
     setLoading(projectsLoading);
   }, [projectsLoading]);
+
+  const addProjectHandler = async (e, project) => {
+    e.preventDefault();
+
+    try {
+      await addProject({ variables: { ...project } });
+    } catch (error) {
+      console.error("addProject Error: ", error.message);
+      setError(error.message);
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+
+  const deletHandler = (e, projectId) => {
+    e.preventDefault();
+    deleteProject({ variables: { id: projectId } });
+  };
 
   return (
     <div>
       {loading ? (
         <Spinner />
-      ) : error ? (
-        <h2>{`Error Projects Screen: ${error.message}`}</h2>
+      ) : projectsError ? (
+        <h2>{`Error Projects Screen: ${projectsError.message}`}</h2>
       ) : (
-        <div className="row mb-3">
-          {projectsDdata.projects.map((project) => (
-            <Project key={project.id} project={project} />
-          ))}
+        <div>
+          <div className="row">
+            <div className="col">
+              <AddProjectModal handleSubmit={addProjectHandler} />
+            </div>
+          </div>
+          <div className="row mb-3">
+            {projectsDdata.projects.map((project) => (
+              <Project
+                key={project.id}
+                project={project}
+                deletHandler={deletHandler}
+              />
+            ))}
+          </div>
         </div>
       )}
     </div>
