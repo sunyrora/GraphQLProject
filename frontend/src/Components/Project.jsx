493bc39e00bc34cf58c233c1522f473d074e1d2d
aurora13 <aurora13@naver.com>
2022-06-21 14:26:44 +0200

    Modified Project Component style

--- frontend/src/Components/Project.jsx
+++ frontend/src/Components/Project.jsx
@@ -2,28 +2,27 @@ const Project = ({ project, deletHandler }) => {
   return (
     <div className="col mb-3 col-sm-4">
       <div className="card m-1 h-100">
-        <a
-          href={`/project/${project.id}`}
-          className="project-name w-100 d-flex flex-column justify-content-center"
-        >
-          <div className="card-header text-truncate">{project.name}</div>
-        </a>
-        <div className="card-body d-flex d-flex flex-column justify-content-start align-items-center p-1">
+        <div className="card-header">
+          <div className="project-name">
+            <a className="" href={`/project/${project.id}`}>
+              {project.name}
+            </a>
+            <span className="badge project-status bg-info text-wrap small">
+              {project.status}
+            </span>
+          </div>
+        </div>
+        <div className="card-body">
           <a
             href={`/project/${project.id}`}
-            className="btn project-description w-100 d-flex flex-column justify-content-center"
+            className="btn project-description w-100"
           >
             <div className="card-text mb-1 text-truncate">
               {project.description}
             </div>
           </a>
-          <div>
-            <span className="badge bg-info text-wrap small">
-              {project.status}
-            </span>
-          </div>
         </div>
-        <div className="card-footer d-flex justify-content-center">
+        <div className="card-footer">
           <button className="btn btn-outline-success small">Edit</button>
           <button
             className="btn btn-outline-success small"
