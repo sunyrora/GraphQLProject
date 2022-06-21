bf491223aa9510e938c327afae058db8019f8bbd
sunyrora <sunyrora@gmail.com>
2022-06-21 14:51:33 +0200

    1. Modified again Project component style
    2. change the way of pass  Project detail(ProjectScreen) Link params.

--- frontend/src/Components/Project.jsx
+++ frontend/src/Components/Project.jsx
@@ -1,32 +1,55 @@
-const Project = ({ project, deletHandler }) => {
+import { Link } from "react-router-dom";
+
+const Project = ({ project, deleteHandler }) => {
+  const linkProps = {
+    to: `/project/${project.id}`,
+    id: project.id,
+    state: { testState: "testState" },
+  };
+
   return (
-    <div className="col mb-3 col-sm-4">
+    <div className="col mb-3 col-md-4">
       <div className="card m-1 h-100">
         <div className="card-header">
           <div className="project-name">
-            <a className="" href={`/project/${project.id}`}>
+            <Link
+              to={linkProps.to}
+              id={linkProps.id}
+              state={{ ...linkProps.state }}
+            >
               {project.name}
-            </a>
+            </Link>
+
+            {/* <a className="" href={`/project/${project.id}`}>
+              {project.name}
+            </a> */}
             <span className="badge project-status bg-info text-wrap small">
               {project.status}
             </span>
           </div>
         </div>
-        <div className="card-body">
-          <a
+        <div className="card-body projects-screen">
+          {/* <a
             href={`/project/${project.id}`}
+            className="btn project-description w-100"
+          > */}
+          <Link
+            to={linkProps.to}
+            id={linkProps.id}
+            state={linkProps.state}
             className="btn project-description w-100"
           >
             <div className="card-text mb-1 text-truncate">
               {project.description}
             </div>
-          </a>
+          </Link>
+          {/* </a> */}
         </div>
-        <div className="card-footer">
+        <div className="card-footer projects-screen">
           <button className="btn btn-outline-success small">Edit</button>
           <button
             className="btn btn-outline-success small"
-            onClick={(e) => deletHandler(e, project.id)}
+            onClick={(e) => deleteHandler(e, project.id)}
           >
             Delete
           </button>
